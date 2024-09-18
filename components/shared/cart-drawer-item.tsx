b1f4840c81296f81d518cd17cbf408067850c488
Daniil Durnev <dadurn@taltech.ee>
2024-09-18 23:16:11 +0300

    fff

--- components/shared/cart-drawer-item.tsx
+++ components/shared/cart-drawer-item.tsx
@@ -34,7 +34,7 @@ export const CartDrawerItem: React.FC<Props> = ({
     >
       <CartItem.Image src={imageUrl} />
 
-      <div className={"flex-1"}>
+      <div className="flex-1">
         <CartItem.Info name={name} details={details} />
         <hr className="my-3" />
         <div className={"flex items-center justify-between"}>
